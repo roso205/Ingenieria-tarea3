8d4b82f5631fa35b5fdbf4e9b9c7a1e1c82c579f
Roso Peñaranda <roso205@gmail.com>
2013-10-03 11:37:55 -0430

    se modifican las clases Conexion, Plan, GestionarPlan

--- Ingenieria_tarea3/src/Conexion.java
+++ Ingenieria_tarea3/src/Conexion.java
@@ -5,8 +5,27 @@
 
 /**
  *
- * 
+ * @author roso
  */
+import java.sql.*;
+//creamos la clase
 public class Conexion {
+   
+    private  String url = "jdbc:postgresql://localhost:5432/postgres2";
+    private String user = "postgres";
+    private String pass = "postgres";
+    private Connection cn;
+     
+    public Connection getConexion() {
+        try {
     
+            Class.forName("org.postgresql.Driver");
+            cn = DriverManager.getConnection(url,user,pass);
+        } catch (Exception e) {
+               e.printStackTrace();
+        }
+        return cn;
+    }
+    
+
 }
